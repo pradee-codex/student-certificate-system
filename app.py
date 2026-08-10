b60ffec090bdcaa6816a5991b4ec2a3885c06982
Pradeep <pradeep@gmail.com>
2026-08-10 20:39:43 +0530

    Prepare project for deployment

--- app.py
+++ app.py
@@ -57,7 +57,14 @@ os.makedirs(PROFILE_FOLDER, exist_ok=True)
 # MySQL
 # ==========================
 
-mysql = MySQL(app)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = ""
+app.config["MYSQL_DB"] = "student_certificate_system"
+app.config["MYSQL_PORT"] = 3306
+
+mysql = MySQL()
+mysql.init_app(app)
 
 # ==========================
 # Home
--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+
+import os
+
 class Config:
-    SECRET_KEY = "student_certificate_secret"
+
+    SECRET_KEY = "student-certificate-system-secret-key-2026"
 
     MYSQL_HOST = "localhost"
     MYSQL_USER = "root"
     MYSQL_PASSWORD = ""
     MYSQL_DB = "student_certificate_system"
+    MYSQL_PORT = 3306
