b47090fd9186fe39e5def8a1b36cc00bbb644fcc
Pradeep <pradeep@gmail.com>
2026-08-10 22:13:35 +0530

    Fix MySQL environment configuration

--- app.py
+++ app.py
@@ -57,11 +57,13 @@ os.makedirs(PROFILE_FOLDER, exist_ok=True)
 # MySQL
 # ==========================
 
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "student_certificate_system"
-app.config["MYSQL_PORT"] = 3306
+
+
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DATABASE")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", 3306))
 
 mysql = MySQL()
 mysql.init_app(app)
