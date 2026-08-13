e362f397a00c5d8740dfca65f276e1f98689b734
Pradeep <pradeep@gmail.com>
2026-08-13 17:58:24 +0530

    Fix MySQL connection

--- app.py
+++ app.py
@@ -60,13 +60,11 @@ os.makedirs(PROFILE_FOLDER, exist_ok=True)
 
 
 
-app = Flask(__name__)
-
-app.config["MYSQL_HOST"] = "mysql-2712797e-vertex00096-7dca.l.aivencloud.com"
-app.config["MYSQL_USER"] = "avnadmind"
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+app.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
 app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
-app.config["MYSQL_DB"] = "defaultdb"
-app.config["MYSQL_PORT"] = 17135
+app.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+app.config["MYSQL_PORT"] = int(os.getenv("MYSQL_PORT", "3306"))
 
 app.config["MYSQL_SSL"] = {
     "ssl": {}
